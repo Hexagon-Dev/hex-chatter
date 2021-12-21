235e13a8422df8a71c11d31f3b650047c0463685
hexagon14 <hexagon14@mail.ru>
2021-12-21 17:49:47 +0200

    first commit

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SendMessageRequest;
+use App\Jobs\SendMessage;
+use App\Models\Group;
+use App\Models\User;
+
+class MessageController extends Controller
+{
+    public function send(SendMessageRequest $request, int $id, bool $is_group)
+    {
+        if ($is_group) {
+            $recepient = Group::query()->findOrFail($id);
+        } else {
+            $recepient = User::query()->findOrFail($id);
+        }
+
+        SendMessage::dispatchAfterResponse($request->validated()['text'], $id, $is_group);
+
+    }
+}
